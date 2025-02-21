216a1c9cd3bb76f7cad7fc37e3ce3e1a82eabddc
Jason Howell <howell.jasong@gmail.com>
2025-02-21 15:58:37 -0600

    added contact page and form

---
+++ javascript.js
@@ -0,0 +1,13 @@
+// let greeting = "Hello World";
+
+// console.log(greeting);
+
+
+const form = document.querySelector('#form')
+const submitButton = document.querySelector('#submit')
+
+form.addEventListener('submit', (e) => {
+  submitButton.disabled = true
+  e.preventDefault()
+  window.location.href = window.location.origin + '/success.html'
+})
